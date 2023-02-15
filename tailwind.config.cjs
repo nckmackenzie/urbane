ace91b2d68c688c63724540c838699db22001148
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-15 16:43:03 +0300

    add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#475BE8',
+        'light-text': '#11142D',
+        'light-text-secondary': '#808191',
+        'dark-text': '#EFEFEF',
+        'dark-text-secondary': '#6F767E',
+        'light-bg': '#F4F4F4',
+        'dark-bg': '#111315',
+        'app-light-bg': '#FCFCFC',
+        'app-dark-bg': '#1A1D1F',
+      },
+    },
+    fontFamily: {
+      bodytext: ['Manrope', 'sans-serif'],
+    },
   },
   plugins: [],
 };
