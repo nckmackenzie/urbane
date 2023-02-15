3ea052acc114b8cf44a8f4a22aee78b1a8c34090
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-15 22:18:38 +0300

    NavLink item

---
+++ src/components/layout/NavItem.jsx
@@ -0,0 +1,24 @@
+import { Link, useLocation } from 'react-router-dom';
+import { useTheme } from '../../context/ThemeProvider';
+
+function NavItem({ path, name, icon }) {
+  const { textColorSec } = useTheme();
+  const location = useLocation();
+  return (
+    <li className="">
+      <Link
+        to={path}
+        className={`flex items-center gap-1 ${textColorSec} rounded-md p-4 ${
+          path === location.pathname
+            ? 'bg-primary text-app-light-bg'
+            : 'hover:bg-primary hover:text-app-light-bg'
+        } `}
+      >
+        <span>{icon}</span>
+        <span className={`text-lg font-medium text-inh`}>{name}</span>
+      </Link>
+    </li>
+  );
+}
+
+export default NavItem;
