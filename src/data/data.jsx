8d409069baa92231a7274afe898baae63019b6d8
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-15 22:19:04 +0300

    add size to icons

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -4,10 +4,18 @@ import { BsPeople } from 'react-icons/bs';
 import { MdOutlineAccountCircle } from 'react-icons/md';
 
 export const navigation = [
-  { name: 'Dashboard', path: '/', icon: <AiOutlineDashboard /> },
-  { name: 'Property', path: '/properties', icon: <BiBuildingHouse /> },
-  { name: 'Agent', path: '/agents', icon: <BsPeople /> },
-  { name: 'Review', path: '/reviews', icon: <AiOutlineStar /> },
-  { name: 'Message', path: '/messages', icon: <BiMessageRounded /> },
-  { name: 'My Profile', path: '/profile', icon: <MdOutlineAccountCircle /> },
+  { name: 'Dashboard', path: '/', icon: <AiOutlineDashboard size={24} /> },
+  {
+    name: 'Property',
+    path: '/properties',
+    icon: <BiBuildingHouse size={24} />,
+  },
+  { name: 'Agent', path: '/agents', icon: <BsPeople size={24} /> },
+  { name: 'Review', path: '/reviews', icon: <AiOutlineStar size={24} /> },
+  { name: 'Message', path: '/messages', icon: <BiMessageRounded size={24} /> },
+  {
+    name: 'My Profile',
+    path: '/profile',
+    icon: <MdOutlineAccountCircle size={24} />,
+  },
 ];
