2f6c58a79cd784ead6419a836dd07385b13661f8
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-16 16:44:46 +0300

    add TotalRevenue and Property Referals components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,21 @@
+import {
+  PropertyReferals,
+  Stats,
+  TotalRevenue,
+} from '../components/pages/dashboard';
+import { PageTitle } from '../components/ui';
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <PageTitle title="Dashboard" />
+      <Stats />
+      <div className="flex flex-col md:flex-row gap-6 mt-4">
+        <TotalRevenue />
+        <PropertyReferals />
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
