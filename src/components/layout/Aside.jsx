566ec1e27a5de52ec60684da40bcb9e77846198c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-16 08:52:50 +0300

    render nav items

--- src/components/layout/Aside.jsx
+++ src/components/layout/Aside.jsx
@@ -1,13 +1,23 @@
 import logo from '../../assets/logo-128px.png';
 import { useTheme } from '../../context/ThemeProvider';
+import { navigation } from '../../data/data';
+import NavItem from './NavItem';
 function Aside() {
   const { appBgColor, textColor } = useTheme();
+
   return (
     <aside className={`lg:w-72 ${appBgColor}`}>
       <div className="h-24 flex items-center justify-center gap-2">
         <img src={logo} alt="Urbane logo" className="w-16 h-16" />
         <h1 className={`text-2xl uppercase font-bold ${textColor}`}>Urbane</h1>
       </div>
+      <nav>
+        <ul className="p-2 flex flex-col gap-2">
+          {navigation?.map(nav => (
+            <NavItem key={nav.name} {...nav} />
+          ))}
+        </ul>
+      </nav>
     </aside>
   );
 }
