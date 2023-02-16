b5605eb1cb131f0c83eb8afa72bf00c465765e97
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-16 09:57:08 +0300

    SearchInput component

---
+++ src/components/ui/SearchInput.jsx
@@ -0,0 +1,26 @@
+import { BiSearch } from 'react-icons/bi';
+import { useTheme } from '../../context/ThemeProvider';
+
+function SearchInput() {
+  const { bgColor, textColorSec, textColor } = useTheme();
+  return (
+    <form
+      action=""
+      role="search"
+      className={`${bgColor} relative w-1/4 ${textColor}`}
+    >
+      <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+        <BiSearch />
+      </span>
+      <input
+        type="search"
+        name="q"
+        className={`w-full py-2 text-sm text-white bg-inherit rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900 placeholder:${textColorSec}`}
+        placeholder="Search property,customer,etc..."
+        autocomplete="off"
+      />
+    </form>
+  );
+}
+
+export default SearchInput;
