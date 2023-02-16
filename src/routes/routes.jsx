1d353b703576e2f12a5b86c36f161cce66f77b9c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-16 09:27:46 +0300

    add more page routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../components/layout/MainLayout';
 import { Login, SignIn } from '../pages/auth';
-import Dashboard from '../pages/Dashboard';
+import {
+  Dashboard,
+  Properties,
+  Agents,
+  Messages,
+  Reviews,
+  Profile,
+} from '../pages';
 
 export const router = createBrowserRouter([
   {
@@ -19,6 +26,26 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <Dashboard />,
+      },
+      {
+        path: '/properties',
+        element: <Properties />,
+      },
+      {
+        path: '/agents',
+        element: <Agents />,
+      },
+      {
+        path: '/messages',
+        element: <Messages />,
+      },
+      {
+        path: '/reviews',
+        element: <Reviews />,
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
       },
     ],
   },
