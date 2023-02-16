36ebd5463a2ce6bf787d42e679fa0fa54953c5c4
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-16 11:33:45 +0300

    handle menu hiding on click outside user menu

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import { useTheme } from '../../context/ThemeProvider';
+import { useUserMenu } from '../../context/UserMenuProvider';
 import Aside from './Aside';
+import Header from './Header';
 function MainLayout() {
-  const { appBgColor, bgColor } = useTheme();
+  const { bgColor } = useTheme();
+  const { hideMenu } = useUserMenu();
+
+  const onClickHandler = e => {
+    if (e.target.classList.contains('usermenu')) return;
+    hideMenu();
+  };
   return (
-    <div className="lg:flex">
+    <div className="lg:flex" onClick={onClickHandler}>
       <Aside />
-      <main className={`lg:flex-1 bg-green-200 min-h-[100dvh] ${bgColor}`}>
-        <header className={`h-24 ${appBgColor}`}></header>
+      <main className={`lg:flex-1 min-h-[100dvh] ${bgColor}`}>
+        <Header />
         <div className="p-4 md:p-6">
           <Outlet />
         </div>
