addc1f851f3fb5c51b89cc92a63bd31762f5a563
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-16 10:21:18 +0300

    hidden on mobile devices

--- src/components/layout/Aside.jsx
+++ src/components/layout/Aside.jsx
@@ -6,7 +6,7 @@ function Aside() {
   const { appBgColor, textColor } = useTheme();
 
   return (
-    <aside className={`lg:w-72 ${appBgColor}`}>
+    <aside className={`hidden lg:block lg:w-72 ${appBgColor}`}>
       <div className="h-24 flex items-center justify-center gap-2">
         <img src={logo} alt="Urbane logo" className="w-16 h-16" />
         <h1 className={`text-2xl uppercase font-bold ${textColor}`}>Urbane</h1>
