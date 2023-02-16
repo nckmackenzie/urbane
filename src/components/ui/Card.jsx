18b332c81bfd27e210164964705b9474b4661d12
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-16 19:15:11 +0300

    add title prop and title

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,8 +1,11 @@
 import { useTheme } from '../../context/ThemeProvider';
-function Card({ className, children }) {
-  const { appBgColor } = useTheme();
+function Card({ className, children, title }) {
+  const { appBgColor, textColor } = useTheme();
   return (
     <div className={`overflow-hidden  shadow p-4 ${appBgColor} ${className}`}>
+      {title && (
+        <div className={`${textColor} text-lg font-semibold mb-6`}>{title}</div>
+      )}
       {children}
     </div>
   );
