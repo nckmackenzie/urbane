5e87e5bbec800b13710e4c0b58051d768722c512
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-16 09:24:17 +0300

    export pages components

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Messages from './Messages';
+import Agents from './Agents';
+import Properties from './Properties';
+import Profile from './Profile';
+import Dashboard from './Dashboard';
+import Reviews from './Reviews';
+
+export { Messages, Agents, Properties, Profile, Dashboard, Reviews };
