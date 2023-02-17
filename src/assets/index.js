af67afb4d8272e6a01c7a79906568331d07392d5
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-17 12:27:02 +0300

    add more images

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,31 @@ import agent2 from './agent2.jpg';
 import agent3 from './agent4.jpg';
 import agent4 from './agent5.jpg';
 import agent5 from './agent6.jpg';
+import villa1 from './villa1.jpg';
+import villa2 from './villa2.jpg';
+import villa3 from './villa3.jpg';
+import villa4 from './villa4.jpg';
+import villa5 from './villa5.jpg';
+import apartment1 from './apartment1.jpg';
+import apartment2 from './apartment2.jpg';
+import apartment3 from './apartment3.jpg';
+import apartment4 from './apartment4.jpg';
+import apartment5 from './apartment5.jpg';
 
-export { agent1, agent2, agent3, agent4, agent5 };
+export {
+  agent1,
+  agent2,
+  agent3,
+  agent4,
+  agent5,
+  villa1,
+  villa2,
+  villa3,
+  villa4,
+  villa5,
+  apartment1,
+  apartment2,
+  apartment3,
+  apartment4,
+  apartment5,
+};
