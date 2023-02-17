76c742b582ba6ecd59cc7a676137df9b0c57a711
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-17 10:39:39 +0300

    agent avatars

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import agent1 from './agent1.jpg';
+import agent2 from './agent2.jpg';
+import agent3 from './agent4.jpg';
+import agent4 from './agent5.jpg';
+import agent5 from './agent6.jpg';
+
+export { agent1, agent2, agent3, agent4, agent5 };
