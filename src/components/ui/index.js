cc0d8246a204af438ac852c843e5c0e1d9bc3b0a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-17 12:29:10 +0300

    CardActionButton component

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,6 @@
 import SearchInput from './SearchInput';
 import Card from './Card';
 import PageTitle from './PageTitle';
+import CardActionButton from './CardActionButton';
 
-export { SearchInput, Card, PageTitle };
+export { SearchInput, Card, PageTitle, CardActionButton };
