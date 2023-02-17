c76b3e5de0f7746c1b7867e111930feb4f43f63d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-02-17 10:37:30 +0300

    Add header options

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,10 +1,20 @@
 import { useTheme } from '../../context/ThemeProvider';
-function Card({ className, children, title }) {
+function Card({ className, children, title, headerAction, rounded }) {
   const { appBgColor, textColor } = useTheme();
   return (
-    <div className={`overflow-hidden  shadow p-4 ${appBgColor} ${className}`}>
-      {title && (
+    <div
+      className={`overflow-hidden  shadow p-4 ${appBgColor} ${className} ${
+        rounded ? 'rounded-2xl' : ''
+      }`}
+    >
+      {title && !headerAction && (
         <div className={`${textColor} text-lg font-semibold mb-6`}>{title}</div>
+      )}
+      {title && headerAction && (
+        <div className="flex items-center justify-between mb-6">
+          <div className={`${textColor} text-lg font-semibold`}>{title}</div>
+          {headerAction}
+        </div>
       )}
       {children}
     </div>
